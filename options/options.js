ade15964126b7f29a1aced086ef55cf52235401e
Crowdbotics <engineering@crowdbotics.com>
2023-04-02 00:19:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-mountain-dev-73622",
+  url: "https://white-mountain-dev-73622.botics.co",
+  api: "https://white-mountain-dev-73622.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
